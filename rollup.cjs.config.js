cde05729fffe03fb38e3de80cbac17d21aa5307d
Luke Tchang <ltchang@stanford.edu>
2023-01-06 18:08:11 -0700

    polyfill rollup

--- rollup.cjs.config.js
+++ rollup.cjs.config.js
@@ -1,16 +1,19 @@
 import fs from "fs";
 import { builtinModules as builtin } from "module";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 const pkg = JSON.parse(fs.readFileSync("./package.json"));
 
 export default {
-    input: "./main.js",
-    output: {
-        file: "build/main.cjs",
-        format: "cjs",
-    },
-    external: [
-        ...Object.keys(pkg.dependencies),
-        ...builtin,
-    ]
+  input: "./main.js",
+  output: {
+    file: "build/main.cjs",
+    format: "cjs",
+  },
+  external: [...Object.keys(pkg.dependencies), ...builtin],
+  plugins: [
+    nodePolyfills({
+      include: null,
+    }),
+  ],
 };
