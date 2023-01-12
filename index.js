0f7c7cd18bd553f206790e969875a0ba342d6908
Sladuca <sladuca777@gmail.com>
2023-01-12 18:55:27 -0500

    force npm to bump

--- index.js
+++ index.js
@@ -13,3 +13,4 @@ exports.poseidon_slow = require("./src/poseidon_slow");
 exports.smt_hashes_poseidon = require("./src/smt_hashes_poseidon");
 exports.smt = require("./src/smt");
 exports.smt_memdb = require("./src/smt_memdb");
+
