3c6733835230a627ceaeb3bf5433621f37ae03dd
Sladuca <sladuca777@gmail.com>
2023-01-12 16:43:52 -0500

    remove trailing slash

--- src/pedersenHash.js
+++ src/pedersenHash.js
@@ -2,7 +2,7 @@ const babyJub = require("./babyjub");
 const createBlakeHash = require("blake-hash");
 const blake2b = require("blake2b");
 const Scalar = require("ffjavascript").Scalar;
-const Buffer = require("buffer/").Buffer;
+const Buffer = require("buffer").Buffer;
 
 const GENPOINT_PREFIX = "PedersenGenerator";
 const windowSize = 4;
